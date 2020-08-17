3c640314e27abd26efc50bbc4706222358bece0f
paramagicdev <konnor5456@gmail.com>
2020-08-17 17:51:20 -0400

    working on adidng rollup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 import type { SnowpackPluginFactory } from "snowpack";
+const rollup = require("rollup")
 
-const plugin: SnowpackPluginFactory = (config, options) => ({
-  name: "snowpack-plugin-rollup-bundle",
-  input: [".js"],
-  async build({ contents, fileExt, filePath, isDev }: any) {
-    console.table({ contents, fileExt, filePath, isDev });
-    return contents;
-  },
-});
+async function rollupBuild({inputOptions, outputOptions}) {
+  const bundle = await rollup.rollup(inputOptions)
+  const { output } = await bundle.generate(outputOptions)
+
+  for (const chunkOrAsset of output) {
+    if (chunkOrAsset.type === "asset") {
+      // assets
+    } else {
+     // chunks
+    }
+  }
+
+  await bundle.write(outputOptions)
+}
+
+const plugin: SnowpackPluginFactory = (config, options) => {
+  config.buildOptions.minify = false // Rollup will handle this
+
+  return {
+    name: "snowpack-plugin-rollup-bundle",
+    input: ["*"],
+    async optimize({ buildDirectory }) {
+
+    }
+  }
+};
 
 export default plugin;
