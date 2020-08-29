7a7ad7b033d8f8ebe949196befcacf1098e35cca
paramagicdev <konnor5456@gmail.com>
2020-08-29 16:41:07 -0400

    feat: working on an import hash resolver

--- src/importHashResolver.js
+++ src/importHashResolver.js
@@ -1,5 +1,16 @@
-// export function importHashResolver(source) {
-//   return source.replace(/from\s*['"].*\.(\w+)\.proxy\.js['"]/g, (fullMatch) => {
-//    return fullMatch.replace(".proxy.js", "");
-//  });
-// }
+const path = import("path")
+
+export function importHashResolver({currentFilePath, fileName, manifestKeys}) {
+  const oldFiles = manifestKeys.map((file) => path.relative(currentFilePath, file))
+  return fileName.replace(
+    /from\s*['"]\(.?.*\)['"]/g,
+    (oldPath) => {
+      if (!oldFiles.contains(oldPath)) {
+        return oldPath // Do nothing
+      }
+
+      const substituteFile = path.relative(currentFilePath, newFile)
+      return oldPath.replace(substituteFile);
+    }
+  );
+}
