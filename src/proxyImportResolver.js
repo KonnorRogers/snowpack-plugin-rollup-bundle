8b0c65a1dbd31bcf9ea98a1425ad14b1963ca2cc
paramagicdev <konnor5456@gmail.com>
2020-08-30 01:45:14 -0400

    chore: clean up proxy import resolver

--- src/proxyImportResolver.js
+++ src/proxyImportResolver.js
@@ -1,6 +1,9 @@
 // https://github.com/pikapkg/snowpack/blob/master/plugins/plugin-webpack/plugins/proxy-import-resolve.js
-export function proxyImportResolver(source) {
-  return source.replace(/from\s*['"].*\.(\w+)\.proxy\.js['"]/g, (fullMatch) => {
+export function proxyImportResolver(source, { ext = /.*/ }) {
+  const fromStatement = /from\s*['"].*\./
+  const proxyJs = /\.proxy\.js['"]/
+  const regex = new RegExp(fromStatement + ext + proxyJs, "g")
+  return source.replace(regex, (fullMatch) => {
     return fullMatch.replace(".proxy.js", "");
   });
 }
