d819020c37fa40a96a3483ae4af946a10c110249
paramagicdev <konnor5456@gmail.com>
2020-08-29 23:29:37 -0400

    chore: abstract build process

--- __tests__/build.test.js
+++ __tests__/build.test.js
@@ -3,16 +3,15 @@ const childProcess = require("child_process")
 const path = require("path")
 const fs = require("fs")
 
-const consoleSpy = jest.spyOn(console, "log").mockImplementation()
+const buildDir = path.join(process.env.EXAMPLE_DIR, "build")
 
-test('Should produce a manifest.json', () => {
-  const buildDir = path.join(process.env.EXAMPLE_DIR, "build")
+beforeAll(() => {
   childProcess.execSync(`rm -rf ${buildDir}`)
-  const manifestFile = path.join(buildDir, "manifest.json")
-
-  // build without error
   process.chdir(process.env.EXAMPLE_DIR)
   childProcess.execSync("yarn build")
+})
 
-  fs.existsSync(manifestFile)
+test('Should produce a manifest.json', () => {
+  const manifestFile = path.join(buildDir, "manifest.json")
+  expect(fs.existsSync(manifestFile)).toBe(true)
 });
