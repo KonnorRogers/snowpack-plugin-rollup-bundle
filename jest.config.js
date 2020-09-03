bb5524b167929d78f3623b54a297f2c41ad620f1
ParamagicDev <konnor5456@gmail.com>
2020-09-03 13:44:22 -0400

    working on stuff

--- jest.config.js
+++ jest.config.js
@@ -193,4 +193,12 @@ module.exports = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
+  watchPlugins: [
+    [
+      "./jest-watch-plugin-rebuild.js",
+      {
+        command: "yarn build"
+      }
+    ],
+  ]
 };
