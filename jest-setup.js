bc5301639e5767ba4a34dd6f24963be76bde7c51
paramagicdev <konnor5456@gmail.com>
2020-09-05 17:24:44 -0400

    move building to jest setup

--- jest-setup.js
+++ jest-setup.js
@@ -1,3 +1,6 @@
+const path = require("path")
+const process = require("process")
+
 import { shellRun } from "./src/utils"
 
 export default async function globalSetup() {
@@ -6,5 +9,17 @@ export default async function globalSetup() {
 }
 
 async function build() {
-  return shellRun("yarn build")
+  shellRun("yarn build")
+
+  const exampleDir = path.resolve("__tests__", "example_dir");
+  const buildDir = path.join(exampleDir, "build");
+
+  // Remove and Rebuild the build directory
+  shellRun(`rm -rf ${buildDir}`);
+
+  process.chdir(exampleDir);
+
+  const { status } = shellRun("yarn install --force && yarn build");
+
+  if (status !== 0) throw "An error occurred building the test directory";
 }
