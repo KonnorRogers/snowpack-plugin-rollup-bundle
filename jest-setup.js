df8eb068d67b564b9b6ed33786c0b44214a7c7fe
paramagicdev <konnor5456@gmail.com>
2020-09-05 17:31:34 -0400

    retool jest-setup.js

--- jest-setup.js
+++ jest-setup.js
@@ -1,25 +1,30 @@
-const path = require("path")
-const process = require("process")
+const path = require("path");
+const process = require("process");
 
-import { shellRun } from "./src/utils"
+import { shellRun } from "./src/utils";
 
 export default async function globalSetup() {
-  const { status } = await build()
-  if (status !== 0) throw "Unable to build your package"
+  const buildPkg = await build();
+  if (buildPkg.status !== 0) {
+    throw "Unable to build your package";
+  }
+
+  const buildTestDir = await buildTestDirectory();
+  if (buildTestDir.status !== 0) {
+    throw "An error occurred building the test directory";
+  }
 }
 
 async function build() {
-  shellRun("yarn build")
+  return shellRun("yarn build");
+}
 
+async function buildTestDirectory() {
   const exampleDir = path.resolve("__tests__", "example_dir");
   const buildDir = path.join(exampleDir, "build");
 
   // Remove and Rebuild the build directory
   shellRun(`rm -rf ${buildDir}`);
 
-  process.chdir(exampleDir);
-
-  const { status } = shellRun("yarn install --force && yarn build");
-
-  if (status !== 0) throw "An error occurred building the test directory";
+  return shellRun(`cd ${exampleDir} && yarn install --force && yarn build`);
 }
