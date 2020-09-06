50f58355791164a0388b0a21e953437238aab8e4
ParamagicDev <konnor5456@gmail.com>
2020-09-06 15:53:20 -0400

    add manualChunks and remove alias

--- src/options.js
+++ src/options.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 // plugins
-import alias from "@rollup/plugin-alias";
+import resolve from "@rollup/plugin-node-resolve";
 import styles from "rollup-plugin-styles";
 import { terser } from "rollup-plugin-terser";
 import url from "@rollup/plugin-url";
@@ -9,6 +9,7 @@ import url from "@rollup/plugin-url";
 export function defaultInputOptions({ buildDirectory, tmpDir }) {
   return {
     plugins: [
+      resolve({browser: true}),
       styles({
         mode: ["extract"],
         modules: true,
@@ -23,18 +24,6 @@ export function defaultInputOptions({ buildDirectory, tmpDir }) {
         limit: 0, // extract all files
         fileName: "[name].[hash][extname]",
       }),
-      alias({
-        entries: [
-          {
-            find: "/__snowpack__",
-            replacement: path.relative(buildDirectory, "__snowpack__"),
-          },
-          {
-            find: "/web_modules",
-            replacement: path.relative(buildDirectory, "web_modules"),
-          },
-        ],
-      }),
     ],
   };
 }
@@ -43,9 +32,15 @@ export function defaultOutputOptions(buildDirectory) {
   return {
     format: "es",
     plugins: [terser()],
+    manualChunks: (id) => {
+      if (id.includes("node_modules")) {
+        return "vendor"
+      }
+    },
     assetFileNames: "css/[name].[hash].[ext]",
     chunkFileNames: "chunks/[name].[hash].js",
     compact: true,
+    sourceMap: true,
     entryFileNames: "entrypoints/[name].[hash].js",
     dir: buildDirectory,
   };
