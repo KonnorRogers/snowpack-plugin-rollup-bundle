cefbee43694379703e077ad80d059afb8fa465a2
ParamagicDev <konnor5456@gmail.com>
2020-09-06 12:30:54 -0400

    chore: modify test ids

--- __tests__/example_dir/src/javascript/index.js
+++ __tests__/example_dir/src/javascript/index.js
@@ -2,6 +2,7 @@ const js = {
   append: (element) => {
     const div = document.createElement("div")
     div.innerText = "hello from javascript"
+    div.dataset.testId = "javascript"
     element.appendChild(div)
   }
 }
