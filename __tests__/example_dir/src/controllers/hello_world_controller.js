275a2f1c515132c02ae74141c21e7cf71a420eb1
ParamagicDev <konnor5456@gmail.com>
2020-09-06 19:56:22 -0400

    Add stimulus controllers

--- __tests__/example_dir/src/controllers/hello_world_controller.js
+++ __tests__/example_dir/src/controllers/hello_world_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "stimulus";
 
 export default class HelloWorldController extends Controller {
   initialize() {
+    document.querySelector("#hello-world").remove()
     const div = document.createElement("div");
     div.id = "hello-world";
     div.innerText = "Hello World from Stimulus";
