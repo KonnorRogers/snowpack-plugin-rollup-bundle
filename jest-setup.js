4c3427677b369c528de2edad2651137a5afb3276
paramagicdev <konnor5456@gmail.com>
2020-09-12 18:40:10 -0400

    chore: remove building the test directory

--- jest-setup.js
+++ jest-setup.js
@@ -7,23 +7,8 @@ export default async function globalSetup() {
   if (build.status !== 0) {
     throw "Unable to build your package";
   }
-
-  const buildTestDir = await buildTestDirectory();
-  if (buildTestDir.status !== 0) {
-    throw "An error occurred building the test directory";
-  }
 }
 
 async function buildPkg() {
   return shellRun("yarn build");
-}
-
-async function buildTestDirectory() {
-  const exampleDir = path.resolve("__tests__", "example_dir");
-  const buildDir = path.join(exampleDir, "build");
-
-  // Remove and Rebuild the build directory
-  shellRun(`rm -rf ${buildDir}`);
-
-  return shellRun(`cd ${exampleDir} && yarn install --force && yarn build`);
 }
