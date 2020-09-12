8414a304eff6c760d3d797945d70a2974195ebba
ParamagicDev <konnor5456@gmail.com>
2020-09-11 23:09:53 -0400

    found issue, 2 consecutivve builds

--- __tests__/browser.test.js
+++ __tests__/browser.test.js
@@ -26,4 +26,4 @@
 //     return dom
 //   });
 
-test("Should properly load the page with no console errors", async () => {});
+// test("Should properly load the page with no console errors", async () => {});
