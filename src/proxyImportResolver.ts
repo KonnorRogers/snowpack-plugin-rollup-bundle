b7bed14708c03c4bf100c616452f7bd66ec600ba
ParamagicDev <konnor5456@gmail.com>
2020-11-30 15:59:12 -0500

    moving to ts

--- src/proxyImportResolver.ts
+++ src/proxyImportResolver.ts
@@ -1,5 +1,5 @@
 // https://github.com/pikapkg/snowpack/blob/master/plugins/plugin-webpack/plugins/proxy-import-resolve.js
-export function proxyImportResolver(source) {
+export function proxyImportResolver(source: string): string {
   const regex = /import.*['"].*\.(\w+)\.proxy\.js['"]/g;
   return source.replace(regex, (fullMatch, originalExt) => {
     // no JSON plugin loaded
