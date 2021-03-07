360b9c813eb5a51e96bb766a49a4f0cf6313f95e
Nicolae Dascalu <n.dascalu@ceyoniq.com>
2021-03-07 18:22:37 +0100

    improve performance and fix regex reset .proxy.js imports

--- src/proxyImportResolver.js
+++ src/proxyImportResolver.js
@@ -1,6 +1,6 @@
 // https://github.com/pikapkg/snowpack/blob/master/plugins/plugin-webpack/plugins/proxy-import-resolve.js
 export function proxyImportResolver(source) {
-  const regex = /import.*['"].*\.(\w+)\.proxy\.js['"]/g;
+  const regex = /^import.*['"].*\.(\w+)\.proxy\.js['"][;\s]*$/mg;
   return source.replace(regex, (fullMatch, originalExt) => {
     // no JSON plugin loaded
     if (originalExt === "json") {
